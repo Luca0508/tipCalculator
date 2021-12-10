5712632cf40c3f958ccde1968b27cd13e4200077
JuliaHsiao <juliaxiao860508@gmail.com>
2021-12-10 12:31:50 +0800

    2021/12/10 12:31

--- tipsCalculator/ViewController.swift
+++ tipsCalculator/ViewController.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // input textField
+    @IBOutlet weak var moneyInput: UITextField!
+    @IBOutlet weak var tipsInput: UITextField!
+    @IBOutlet weak var numOfPeopleInput: UITextField!
+    
+    
+    // output Label
+    @IBOutlet weak var totalMoneyOutput: UILabel!
+    @IBOutlet weak var tipsOutput: UILabel!
+    @IBOutlet weak var avgOutput: UILabel!
+    
+    var money: Double
+    var tipsPercentage: Double
+    var numOfPeople: Int
+    
+    var total: Double
+    var tips: Double
+    var avg: Double
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
