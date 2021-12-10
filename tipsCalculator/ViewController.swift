aa9b1913596bdbf2e5aff87b1cfc6130d6f6e312
JuliaHsiao <juliaxiao860508@gmail.com>
2021-12-10 11:37:01 +0800

    Initial Commit

---
+++ tipsCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipsCalculator
+//
+//  Created by 蕭鈺蒖 on 2021/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
