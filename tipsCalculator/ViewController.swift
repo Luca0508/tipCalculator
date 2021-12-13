78f5681134e962f395e1588f919e2f63db207f7e
JuliaHsiao <juliaxiao860508@gmail.com>
2021-12-13 23:25:43 +0800

    almost done (stepper?)

--- tipsCalculator/ViewController.swift
+++ tipsCalculator/ViewController.swift
@@ -14,27 +14,110 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipsInput: UITextField!
     @IBOutlet weak var numOfPeopleInput: UITextField!
     
+    // output textField
+    @IBOutlet weak var totalOutputLabel: UILabel!
+    @IBOutlet weak var avgOutputLabel: UILabel!
+    @IBOutlet weak var tipOutputLabel: UILabel!
     
-    // output Label
-    @IBOutlet weak var totalMoneyOutput: UILabel!
-    @IBOutlet weak var tipsOutput: UILabel!
-    @IBOutlet weak var avgOutput: UILabel!
+    @IBOutlet weak var warningLabel: UILabel!
     
-    var money: Double
-    var tipsPercentage: Double
-    var numOfPeople: Int
     
-    var total: Double
-    var tips: Double
-    var avg: Double
+    @IBOutlet weak var peopleStepper: UIStepper!
+    
+    var total: Double = 0.0
+    var tip: Double = 0.0
+    var avg: Double = 0.0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        totalOutputLabel.text = "0"
+        tipOutputLabel.text = "0"
+        avgOutputLabel.text = "0"
+        numOfPeopleInput.text = "1"
+        
+        
+    }
+    
+    @IBAction func ChangeStepperValue(_ sender: UIStepper) {
+        
+        numOfPeopleInput.text = Int(sender.value).description
+    }
+    
+    @IBAction func deleteAll(_ sender: UIButton) {
+        moneyInput.text = ""
+        tipsInput.text = ""
+        
+        peopleStepper.value = 1.0
+        numOfPeopleInput.text = Int(peopleStepper.value).description
+        
+        totalOutputLabel.text = "0"
+        tipOutputLabel.text = "0"
+        avgOutputLabel.text = "0"
+        
+        warningLabel.isHidden = true
+        
+        
     }
     
     
+    @IBAction func calculate(_ sender: UIButton) {
+        refresh()
+    }
+    
+    func refresh(){
+        var totalMoneyInput: Double? {
+            return Double(moneyInput.text!)
+        }
+        var tipsPercentage: Double?{
+            return Double(tipsInput.text!)
+        }
+        
+        
+        var numOfPeople: Double?{
+            return Double(numOfPeopleInput.text!)
+        }
+        
+        moneyInput.resignFirstResponder()
+        tipsInput.resignFirstResponder()
+        numOfPeopleInput.resignFirstResponder()
+        
+        
+        if let totalMoneyInput = totalMoneyInput,
+        let tipsPercentage = tipsPercentage,
+        let numOfPeople = numOfPeople{
+            total = totalMoneyInput * (1 + tipsPercentage/100)
+            tip = totalMoneyInput * tipsPercentage/100
+            avg = total / numOfPeople
+            
+            warningLabel.isHidden = true
+            
+            if tip < 10{
+                warningLabel.text = "客官～大方一點嘛"
+                warningLabel.isHidden = false
+            }else if tip > 20 {
+                warningLabel.text = "客官真棒～謝謝客官！！！"
+                warningLabel.isHidden = false
+            }
+            
+            
+            
+        }else{
+            total = 0.0
+            tip = 0.0
+            avg = 0.0
+            warningLabel.text = "請輸入正確的數字喔！！！"
+            warningLabel.isHidden = false
+        }
+        
+        
+        
+        totalOutputLabel.text = String(format: "%.2f", total)
+        tipOutputLabel.text = String(format: "%.2f",tip)
+        avgOutputLabel.text = String(format: "%.2f",avg)
+            
+    }
 
 
 }
